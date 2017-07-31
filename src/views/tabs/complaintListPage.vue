10d83421bef546745f0b216e185fd8c7c35f35b9
whr <357114638@qq.com>
2017-07-31 14:31:17 +0800

    客诉列表

--- src/views/tabs/complaintListPage.vue
+++ src/views/tabs/complaintListPage.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
         <el-form ref="form" :model="form" label-width="80px">
-
+            <el-form-item label="活动名称">
+                <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="活动名称">
+                <el-input v-model="form.name"></el-input>
+            </el-form-item> <el-form-item label="活动名称">
+            <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="活动名称">
+                <el-input v-model="form.name"></el-input>
+            </el-form-item>
             <el-form-item label="活动名称">
                 <el-input v-model="form.name"></el-input>
             </el-form-item>
