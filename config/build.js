446de5e05ab15fc4bf6fe1967aabc7d2f12dab6c
whr <357114638@qq.com>
2017-08-25 10:22:29 +0800

    ws地址

--- config/build.js
+++ config/build.js
@@ -7,7 +7,7 @@ module.exports =  {
         new webpack.DefinePlugin({//运行时变量
             _BASE_URL:JSON.stringify('api/'),
             _MOCK:false,//模拟数据,
-            _WEBSOCKET:"",
+            _WEBSOCKET:"ws://precsm.zmaxfilm.com:29089",
         }),
         // new webpack.optimize.UglifyJsPlugin({
         //     compress: {
